efe26b08fa4664fdf200c5564124023cd7833354
unknown <adetutu.francisb@gmail.com>
2024-05-10 15:54:26 -0400

    fixing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+export default function App() {
+   const [counter, setCounter]  = React.useState(30);
+   React.useEffect(() =>{
+    if(counter) {
+        setTimeout(() => {
+            setCounter(counter - 1);
+            
+        }, 1000);
+    }
+   }, [counter])
+    return (
+    <div className='app'>
+
+    <p>{counter}</p>
     </div>
-  );
+    )
 }
-
-export default App;
